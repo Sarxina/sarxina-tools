1e4c20a500210aaccd14ffc9d3e5404ba198f0f8
Sarxina <sarxinavt@gmail.com>
2026-04-26 22:48:42 -0400

    Export action layer from package root
    Adds src/actions/index.ts and re-exports it from the root barrel so
    consumers can `import { ActionRegistry, Action, passesFilter, ... }
    from "@sarxina/sarxina-tools"`. Without this, the action layer shipped
    in v0.2.3 was unreachable to downstream packages.

---
+++ src/actions/index.ts
@@ -0,0 +1,4 @@
+export * from "./types.js";
+export { Action } from "./Action.js";
+export { ActionRegistry } from "./ActionRegistry.js";
+export { passesFilter } from "./filterEvaluation.js";
--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export * from "./vts/index.js";
 export * from "./image/index.js";
 export * from "./live2d/index.js";
 export * from "./platforms/index.js";
+export * from "./actions/index.js";
